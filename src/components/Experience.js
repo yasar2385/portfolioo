8f730324bd2e29f13d9ffd5e2cf224ebecbae356
Yasar <61082958+yasar2385@users.noreply.github.com>
2024-07-11 00:29:14 +0530

    Updates

--- src/components/Experience.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const scrollToSection = (id) => {
-  console.log("scrollToSection");
-  const element = document.getElementById(id);
-  if (element) element.scrollIntoView({ behavior: 'smooth' });
-};
